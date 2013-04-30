1da2b92acbae8b874c391695c73c321c0b87dab5
Beau <beau.charman@gmail.com>
2013-04-30 16:03:46 +1000

    Update custom-post-types.php

--- custom-post-types.php
+++ custom-post-types.php
@@ -177,7 +177,7 @@ class LT3_Custom_Post_Type
    * ------------------------------------------------------------------------ */
   public function archive_link()
   {
-    return home_url( '/' . $this->name) ;
+    return home_url( '/' . $this->name ) ;
   }
 
   /**
