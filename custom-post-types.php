5bb6c0d60d8ff354f70bee116afd6fb7a14fe404
Beau Charman <beau.charman@gmail.com>
2013-04-30 21:38:43 +1000

    name change

--- custom-post-types.php
+++ custom-post-types.php
@@ -5,7 +5,7 @@
  * custom-post-types.php
  * @version 2.0 | April 1st 2013
  * @author  Beau Charman | @beaucharman | http://beaucharman.me
- * @link    https://github.com/beaucharman/WordPress-Custom-Post-Types
+ * @link    https://github.com/beaucharman/wordpress-custom-post-types
  * @license MIT license
  *
  * Properties
@@ -20,7 +20,7 @@
  * LT3_Custom_Post_Type class.
  *
  * Configuration guide:
- * https://github.com/beaucharman/WordPress-Custom-Post-Types
+ * https://github.com/beaucharman/wordpress-custom-post-types
  *
  * For more information on registering post types:
  * http://codex.wordpress.org/Function_Reference/register_post_type
